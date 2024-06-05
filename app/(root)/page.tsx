2b8fdb7bda76b88a7abd00728e16eb937e41cfda
Jozef S <franck.vukelic@gmail.com>
2024-06-05 14:32:06 +0700

    Add: Home and PostCard setup

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,33 @@
-import { Button } from "@/components/ui/button";
+"use client";
+//
+// import { useQuery } from "convex/react";
+// import { api } from "@/convex/_generated/api";
+//
+import PodcastCard from "@/components/PodcastCard";
+import LoaderSpinner from "@/components/LoaderSpinner";
+import { podcastData } from "@/constants";
 
+//!
 const Home = () => {
+  // const trendingPodcasts = useQuery(api.podcasts.getTrendingPodcasts);
+  // if(!trendingPodcasts) return <LoaderSpinner />
+
   return (
-    <div className="mt-9 flex flex-col gap-9 ">
+    <div className="mt-9 flex flex-col gap-9 md:overflow-hidden">
       <section className="flex flex-col gap-5">
         <h1 className="text-20 font-bold text-white-1">Trending Podcasts</h1>
-        <Button className="text-white-1 bg-orange-1">Test</Button>
+
+        <div className="podcast_grid">
+           {podcastData?.map(({ id, title, description, imageUrl }) => ( 
+          <PodcastCard
+           key={id}
+              imgUrl={imageUrl}
+              title={title}
+              description={description}
+              podcastId={id} 
+          />
+            ))} 
+        </div>
       </section>
     </div>
   );
