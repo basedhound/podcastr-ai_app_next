ed2ecf4522aee51d19559e36f267df78221ca266
Jozef S <franck.vukelic@gmail.com>
2024-06-07 19:00:31 +0700

    Add: display real podcasts from Convex to Home

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -5,31 +5,26 @@ import { api } from "@/convex/_generated/api";
 //
 import PodcastCard from "@/components/PodcastCard";
 import LoaderSpinner from "@/components/LoaderSpinner";
-import { podcastData } from "@/constants";
+// import { podcastData } from "@/constants"; // For example
 
 //!
 const Home = () => {
+  const trendingPodcasts = useQuery(api.podcasts.getTrendingPodcasts);
   // const tasks = useQuery(api.tasks.get);
-  // const trendingPodcasts = useQuery(api.podcasts.getTrendingPodcasts);
   // if(!trendingPodcasts) return <LoaderSpinner />
 
   return (
     <div className="mt-9 flex flex-col gap-9 md:overflow-hidden">
       <section className="flex flex-col gap-5">
         <h1 className="text-20 font-bold text-white-1">Trending Podcasts</h1>
-
-        <div className="flex min-h-screen flex-col items-center justify-between p-24 text-white-1">
-          {/* {tasks?.map(({ _id, text }) => <div key={_id}>{text}</div>)} */}
-        </div>
-
         <div className="podcast_grid">
-          {podcastData?.map(({ id, title, description, imageUrl }) => (
+          {trendingPodcasts?.map(({ _id, podcastTitle, podcastDescription, imageUrl }) => (
             <PodcastCard
-              key={id}
+              key={_id}
               imgUrl={imageUrl}
-              title={title}
-              description={description}
-              podcastId={id}
+              title={podcastTitle}
+              description={podcastDescription}
+              podcastId={_id}
             />
           ))}
         </div>
