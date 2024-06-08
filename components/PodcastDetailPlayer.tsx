1092e4bcd3f6729d313d865ea2518f0da847706d
Jozef S <franck.vukelic@gmail.com>
2024-06-08 14:26:58 +0700

    Add: PodcastDetailPlayer component

--- components/PodcastDetailPlayer.tsx
+++ components/PodcastDetailPlayer.tsx
@@ -1,9 +1,143 @@
-import React from 'react'
+"use client";
+//
+import { useState } from "react";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { useMutation } from "convex/react";
+//
+import LoaderSpinner from "./LoaderSpinner";
+import { Button } from "./ui/button";
+import { useToast } from "./ui/use-toast";
+//
+import { api } from "@/convex/_generated/api";
+import { useAudio } from '@/providers/AudioProvider';
+import { PodcastDetailPlayerProps } from "@/types";
 
-const PodcastDetailPlayer = () => {
+//!
+const PodcastDetailPlayer = ({
+  audioUrl,
+  podcastTitle,
+  author,
+  imageUrl,
+  podcastId,
+  imageStorageId,
+  audioStorageId,
+  isOwner,
+  authorImageUrl,
+  authorId,
+}: PodcastDetailPlayerProps) => {
+  const router = useRouter();
+  // const { setAudio } = useAudio();
+  const deletePodcast = useMutation(api.podcasts.deletePodcast);
+  const [isDeleting, setIsDeleting] = useState(false);
+  // Success
+  const { toast } = useToast();
+
+  // Delete podcast
+  const handleDelete = async () => {
+    try {
+      await deletePodcast({ podcastId, imageStorageId, audioStorageId });
+      toast({
+        title: "Podcast deleted",
+      });
+      router.push("/");
+    } catch (error) {
+      console.error("Error deleting podcast", error);
+      toast({
+        title: "Error deleting podcast",
+        variant: "destructive",
+      });
+    }
+  };
+
+  // Play
+  const handlePlay = () => {
+    setAudio({
+      title: podcastTitle,
+      audioUrl,
+      imageUrl,
+      author,
+      podcastId,
+    });
+  };
+
+  // If no image url or author image url, show loader
+  if (!imageUrl || !authorImageUrl) return <LoaderSpinner />;
+
   return (
-    <div>PodcastDetailPlayer</div>
-  )
-}
+    <div className="mt-6 flex w-full justify-between max-md:justify-center">
+      <div className="flex flex-col gap-8 max-md:items-center md:flex-row">
+        <Image
+          src={imageUrl}
+          width={250}
+          height={250}
+          alt="Podcast image"
+          className="aspect-square rounded-lg"
+        />
+        <div className="flex w-full flex-col gap-5 max-md:items-center md:gap-9">
+          <article className="flex flex-col gap-2 max-md:items-center">
+            <h1 className="text-32 font-extrabold tracking-[-0.32px] text-white-1">
+              {podcastTitle}
+            </h1>
+            <figure
+              className="flex cursor-pointer items-center gap-2"
+              onClick={() => {
+                router.push(`/profile/${authorId}`);
+              }}
+            >
+              <Image
+                src={authorImageUrl}
+                width={30}
+                height={30}
+                alt="Caster icon"
+                className="size-[30px] rounded-full object-cover"
+              />
+              <h2 className="text-16 font-normal text-white-3">{author}</h2>
+            </figure>
+          </article>
 
-export default PodcastDetailPlayer
+          <Button
+            onClick={handlePlay}
+            className="text-16 w-full max-w-[250px] bg-orange-1 font-extrabold text-white-1"
+          >
+            <Image
+              src="/icons/Play.svg"
+              width={20}
+              height={20}
+              alt="random play"
+            />{" "}
+            &nbsp; Play podcast
+          </Button>
+        </div>
+      </div>
+      {isOwner && (
+        <div className="relative mt-2">
+          <Image
+            src="/icons/three-dots.svg"
+            width={20}
+            height={30}
+            alt="Three dots icon"
+            className="cursor-pointer"
+            onClick={() => setIsDeleting((prev) => !prev)}
+          />
+          {isDeleting && (
+            <div
+              className="absolute -left-32 -top-2 z-10 flex w-32 cursor-pointer justify-center gap-2 rounded-md bg-black-6 py-1.5 hover:bg-black-2"
+              onClick={handleDelete}
+            >
+              <Image
+                src="/icons/delete.svg"
+                width={16}
+                height={16}
+                alt="Delete icon"
+              />
+              <h2 className="text-16 font-normal text-white-1">Delete</h2>
+            </div>
+          )}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default PodcastDetailPlayer;
