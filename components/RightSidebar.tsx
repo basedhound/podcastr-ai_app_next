389d495f6874535d358eb3137475537717d4659c
Jozef S <franck.vukelic@gmail.com>
2024-06-08 14:28:33 +0700

    Add: Clerk button, Header component, topPodcasters section

--- components/RightSidebar.tsx
+++ components/RightSidebar.tsx
@@ -4,26 +4,26 @@ import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { SignedIn, UserButton, useUser } from "@clerk/nextjs";
-// import { useQuery } from 'convex/react';
-// import { api } from '@/convex/_generated/api';
+import { useQuery } from 'convex/react';
+import { api } from '@/convex/_generated/api';
 //
 import { cn } from "@/lib/utils";
 import Header from "./Header";
 import Carousel from "./Carousel";
-// import LoaderSpinner from './LoaderSpinner';
+import LoaderSpinner from './LoaderSpinner';
 // import { useAudio } from '@/providers/AudioProvider';
 
 //!
 const RightSidebar = () => {
-  // const { user } = useUser();
+  const { user } = useUser();
   const router = useRouter();
-  // const topPodcasters = useQuery(api.users.getTopUserByPodcastCount);
+  const topPodcasters = useQuery(api.users.getTopUserByPodcastCount);
 
   // const { audio } = useAudio();
 
   return (
     <section className="right_sidebar">
-      {/*       <SignedIn>
+            <SignedIn>
         <Link href={`/profile/${user?.id}`} className="flex gap-3 pb-12">
           <UserButton />
           <div className="flex w-full items-center justify-between">
@@ -36,15 +36,15 @@ const RightSidebar = () => {
             />
           </div>
         </Link>
-      </SignedIn> */}
+      </SignedIn> 
       <section>
-        <Header /* headerTitle="Fans Like You" */ />
+        <Header headerTitle="Fans Like You" />
         <Carousel /* fansLikeDetail={topPodcasters!} */ />
       </section>
       <section className="flex flex-col gap-8 pt-12">
         <Header /* headerTitle="Top Podcastrs"  */ />
         <div className="flex flex-col gap-6">
-          {/*           {topPodcasters?.slice(0, 3).map((podcaster) => (
+                     {topPodcasters?.slice(0, 3).map((podcaster) => (
             <div key={podcaster._id} className="flex cursor-pointer justify-between" onClick={() => router.push(`/profile/${podcaster.clerkId}`)}>
               <figure className="flex items-center gap-2">
                 <Image
@@ -60,7 +60,7 @@ const RightSidebar = () => {
                 <p className="text-12 font-normal text-white-1">{podcaster.totalPodcasts} podcasts</p>
               </div> 
             </div>
-          ))} */}
+          ))} 
         </div>
       </section>
     </section>
