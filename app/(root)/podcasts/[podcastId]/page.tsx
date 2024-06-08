4fad6c4185b0ab2c9fa87d2173a36fc42a43ec0d
Jozef S <franck.vukelic@gmail.com>
2024-06-08 14:27:33 +0700

    Add: PodcastDetailPayer component

--- app/(root)/podcasts/[podcastId]/page.tsx
+++ app/(root)/podcasts/[podcastId]/page.tsx
@@ -31,10 +31,12 @@ const PodcastDetails = ({
   // If there's no podcast or similar podcasts, show a loading spinner
   if (!similarPodcasts || !podcast) return <LoaderSpinner />;
 
-  return (
+  return  (
     <section className="flex w-full flex-col">
       <header className="mt-9 flex items-center justify-between">
-        <h1 className="text-20 font-bold text-white-1">Currenty Playing</h1>
+        <h1 className="text-20 font-bold text-white-1">
+          Currenty Playing
+        </h1>
         <figure className="flex gap-3">
           <Image
             src="/icons/headphone.svg"
@@ -46,28 +48,22 @@ const PodcastDetails = ({
         </figure>
       </header>
 
-      <PodcastDetailPlayer
-/*         isOwner={isOwner}
+      <PodcastDetailPlayer 
+        isOwner={isOwner}
         podcastId={podcast._id}
-        {...podcast} */
+        {...podcast}
       />
 
-      <p className="text-white-2 text-16 pb-8 pt-[45px] font-medium max-md:text-center">
-        {podcast?.podcastDescription}
-      </p>
+      <p className="text-white-2 text-16 pb-8 pt-[45px] font-medium max-md:text-center">{podcast?.podcastDescription}</p>
 
       <div className="flex flex-col gap-8">
-        <div className="flex flex-col gap-4">
-          <h1 className="text-18 font-bold text-white-1">Transcription</h1>
-          <p className="text-16 font-medium text-white-2">
-            {podcast?.voicePrompt}
-          </p>
+        <div className='flex flex-col gap-4'>
+          <h1 className='text-18 font-bold text-white-1'>Transcription</h1>
+          <p className="text-16 font-medium text-white-2">{podcast?.voicePrompt}</p>
         </div>
-        <div className="flex flex-col gap-4">
-          <h1 className="text-18 font-bold text-white-1">Thumbnail Prompt</h1>
-          <p className="text-16 font-medium text-white-2">
-            {podcast?.imagePrompt}
-          </p>
+        <div className='flex flex-col gap-4'>
+          <h1 className='text-18 font-bold text-white-1'>Thumbnail Prompt</h1>
+          <p className="text-16 font-medium text-white-2">{podcast?.imagePrompt}</p>
         </div>
       </div>
       <section className="mt-8 flex flex-col gap-5">
@@ -75,21 +71,19 @@ const PodcastDetails = ({
 
         {similarPodcasts && similarPodcasts.length > 0 ? (
           <div className="podcast_grid">
-            {similarPodcasts?.map(
-              ({ _id, podcastTitle, podcastDescription, imageUrl }) => (
-                <PodcastCard
-                  key={_id}
-                  imgUrl={imageUrl as string}
-                  title={podcastTitle}
-                  description={podcastDescription}
-                  podcastId={_id}
-                />
-              )
-            )}
+            {similarPodcasts?.map(({ _id, podcastTitle, podcastDescription, imageUrl }) => (
+              <PodcastCard 
+                key={_id}
+                imgUrl={imageUrl as string}
+                title={podcastTitle}
+                description={podcastDescription}
+                podcastId={_id}
+              />
+            ))}
           </div>
         ) : (
-          <>
-            <EmptyState
+          <> 
+            <EmptyState 
               title="No similar podcasts found"
               buttonLink="/discover"
               buttonText="Discover more podcasts"
@@ -97,8 +91,9 @@ const PodcastDetails = ({
           </>
         )}
       </section>
+
     </section>
-  );
+  )
 };
 
 export default PodcastDetails;
