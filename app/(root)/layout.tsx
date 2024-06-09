7f7486054baaaf1b1f0d8a067c2cd8c25b9ef298
Jozef S <franck.vukelic@gmail.com>
2024-06-09 17:08:53 +0700

    Update: format document

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -30,7 +30,6 @@ export default function RootLayout({
             </div>
             <div className="flex flex-col md:pb-14">
               <Toaster />
-
               {children}
             </div>
           </div>
