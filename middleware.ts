5ccc7f105ae1283b61c5b669b5d2eee4c567f544
Jozef S <franck.vukelic@gmail.com>
2024-06-09 17:13:53 +0700

    Add: middleware, change podcasts routes to public

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,7 @@
 
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
-const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)', '/'])
+const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)', '/', '/podcasts(.*)'])
 
 export default clerkMiddleware((auth, req) => {
   if (!isPublicRoute(req)) auth().protect();
